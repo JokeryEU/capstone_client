b94ccc6c1f74cc2c197b86f180fc19f9e3ddbac7
Mihai Ivanov <mikee2988@yahoo.com>
2021-07-09 17:59:25 +0300

    added Payment Method page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import ProfilePage from './pages/ProfilePage'
 import ShippingPage from './pages/ShippingPage'
+import PaymentPage from './pages/PaymentPage'
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path="/shipping" component={ShippingPage} />
+          <Route path="/payment" component={ShippingPage} />
           <Route path="/login" component={LoginPage} />
           <Route path="/register" component={RegisterPage} />
           <Route path="/profile" component={ProfilePage} />
