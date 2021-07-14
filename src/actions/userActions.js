3c84634bcbce527a9465b538804092a7a1dc475b
Mihai Ivanov <mikee2988@yahoo.com>
2021-07-14 23:42:34 +0300

    added delete a user as Admin

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -19,6 +19,9 @@ import {
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
 } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
@@ -178,6 +181,31 @@ export const listUsers = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: USER_LIST_FAIL,
+      payload:
+        error.response && error.response.data
+          ? error.response.data
+          : error.message,
+    })
+  }
+}
+
+export const deleteUser = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_DELETE_REQUEST })
+
+    const { data } = await axios.delete(
+      `${process.env.REACT_APP_API_URL}/users/${id}`,
+      {
+        withCredentials: true,
+      }
+    )
+
+    dispatch({
+      type: USER_DELETE_SUCCESS,
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_DELETE_FAIL,
       payload:
         error.response && error.response.data
           ? error.response.data
