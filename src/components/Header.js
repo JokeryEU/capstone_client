3ac179abdebab25c77a41da8440faacec32d4d65
Mihai Ivanov <mikee2988@yahoo.com>
2021-07-18 23:49:09 +0300

    added Search box without functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,8 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Button,
-  Form,
-  FormControl,
-  Container,
-  Badge,
-} from 'react-bootstrap'
+import { Route } from 'react-router-dom'
+import { Navbar, Nav, NavDropdown, Container, Badge } from 'react-bootstrap'
+import SearchBox from '../components/SearchBox'
 import { logout } from '../actions/userActions'
 
 const Header = () => {
@@ -36,6 +29,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className="ml-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
